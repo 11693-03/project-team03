7f7fe845f6b990f885f3445a988fa27190c5fe94
wrj5518 <wrj5518@gmail.com>
2014-11-09 19:17:55 -0500

    Consumer Initialization!

--- project-team03/src/main/java/edu/cmu/lti/oaqa/pipeline/Consumer.java
+++ project-team03/src/main/java/edu/cmu/lti/oaqa/pipeline/Consumer.java
@@ -1,5 +1,66 @@
 package edu.cmu.lti.oaqa.pipeline;
 
-public class Consumer {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import json.JsonCollectionReaderHelper;
+import json.gson.TestQuestion;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceProcessException;
+
+import edu.cmu.lti.oaqa.type.input.Question;
+
+public class Consumer extends CasConsumer_ImplBase {
+  // File golden_file;//refer to the file that has golden standard answer
+  String goldPath, outputPath;
+
+  List<Question> questions;
+
+  // five kinds of evaluations
+  List<Double[]> precisions;
+
+  List<Double[]> recalls;
+
+  List<Double[]> fmeasures;
+
+  List<Double[]> maps;
+
+  List<Double[]> gmaps;
+
+  File goldenFile;
+  @Override
+  public void initialize() throws ResourceInitializationException {
+    precisions = new ArrayList<Double[]>();
+    recalls = new ArrayList<Double[]>();
+    fmeasures = new ArrayList<Double[]>();
+    maps = new ArrayList<Double[]>();
+    gmaps = new ArrayList<Double[]>();
+
+    goldPath = "/BioASQ-SampleData1B.json";
+    goldenFile = new File((String) getConfigParameterValue("input"));
+
+  }
+
+  @Override
+  public void processCas(CAS aCAS) throws ResourceProcessException {
+
+    // TODO Auto-generated method stub
+    outputPath = "/MyOutput.json";// the path of output file
+    questions = new ArrayList<Question>();
+
+    JCas jcas = null;
+    try {
+      jcas = aCAS.getJCas();
+    } catch (CASException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
 }
